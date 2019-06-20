2a17e50d1ffac5dc0e48c5844783c8f79b0368f3
HarshitJoshi <hjoshi023@gmail.com>
2019-06-20 17:52:30 -0400

    fix blackhatworld source crash

--- sources/blackhatworld.js
+++ sources/blackhatworld.js
@@ -16,6 +16,14 @@ var threadUrls = [
 	'https://www.blackhatworld.com/seo/tunnel-connect-proxies.951125/',
 ];
 
+const userAgents = [
+  'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+  'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0',
+  'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0'
+];
+
 module.exports = {
 
 	homeUrl: 'https://www.blackhatworld.com',
@@ -142,7 +150,7 @@ module.exports = {
 			method: 'GET',
 			url: uri,
 			headers: {
-				'User-Agent': 'proxy-lists-module'
+				'User-Agent': _.sample(userAgents)
 			}
 		}, function(error, response, html) {
 
